f744a918911429d703cb3ceda7530646f09ad971
Richard Harrington <harrington.richard@gmail.com>
2018-02-20 00:19:59 -0500

    first commit after git re-initing
    to get rid of giant file at root commit
    to make github happy

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject thaifood "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/data.csv "0.1.4"]]
+  :main ^:skip-aot thaifood.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
